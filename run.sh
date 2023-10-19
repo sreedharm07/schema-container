3762782dca266375562f23324b98a7421191a1fb
sreedhar <sreedhar70755@gmail.com>
2023-10-19 17:50:20 +0530

    tf

--- run.sh
+++ run.sh
@@ -9,5 +9,5 @@ fi
 
 if [ "$DB_TYPE" == "MONGODB"]; then
   curl -o  rds-combined-ca-bundle.pem https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
-  mongo --ssl --host  $(aws ssm get-parameter  --name db.${ENV}.backend --with-decryption | jq .Parameter.Value | sed -e 's/"//g'):27017 --sslCAFile rds-combined-ca-bundle.pem --username $(aws ssm get-parameter  --name db.${ENV}.username --with-decryption | jq .Parameter.Value | sed -e 's/"//g') --password $(aws ssm get-parameter  --name db.${ENV}.password --with-decryption | jq .Parameter.Value | sed -e 's/"//g') {{COMPONENT}}.js
+  mongo --ssl --host  $(aws ssm get-parameter  --name db.${ENV}.backend --with-decryption | jq .Parameter.Value | sed -e 's/"//g'):27017 --sslCAFile rds-combined-ca-bundle.pem --username $(aws ssm get-parameter  --name db.${ENV}.username --with-decryption | jq .Parameter.Value | sed -e 's/"//g') --password $(aws ssm get-parameter  --name db.${ENV}.password --with-decryption | jq .Parameter.Value | sed -e 's/"//g') $COMPONENT.js
 fi
